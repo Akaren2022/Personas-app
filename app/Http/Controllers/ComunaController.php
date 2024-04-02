09b27e01ea5833b74bcba460be29f3461f0947ce
Aitana <karenaitana21@gmail.com>
2024-04-02 00:24:19 -0500

    Editar Comunas

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\DB;
 
 class ComunaController extends Controller
 {
-
     public function index()
     {
         $comunas = DB::table('tb_comuna')
@@ -19,7 +18,6 @@ class ComunaController extends Controller
         return view('comuna.index', ['comunas' => $comunas]);
     }
 
-
     public function create()
     {
         $municipios = DB::table('tb_municipio')
@@ -27,7 +25,6 @@ class ComunaController extends Controller
         ->get();
         return view('comuna.new', ['municipios' => $municipios]);
     }
-
 
     public function store(Request $request)
     {
@@ -48,15 +45,29 @@ class ComunaController extends Controller
         //
     }
 
-
     public function edit($id)
     {
-        //
+        $comuna = Comuna::find($id);
+
+        $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+        return view('comuna.edit', ['comuna' => $comuna, 'municipios' => $municipios]);
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $comuna = Comuna::find($id);
+
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+        ->get();
+        return view('comuna.index', ['comunas' => $comunas]);
     }
 
     public function destroy($id)
